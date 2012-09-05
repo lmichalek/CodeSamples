8a325d164b89fd75411d25d59b97539ff2d6ef48
Lillian Michalek <lillianfmichalek@gmail.com>
2012-09-04 17:13:26 -0700

    Update bit-manipulation/bits.c

--- bit-manipulation/bits.c
+++ bit-manipulation/bits.c
@@ -2,24 +2,6 @@
  *
  * Lillian Michalek - lmichale
  *
- * This file contains a series of functions that utilize bit patterns to
- * manipulate integers. Each function is limited to using only certain types of 
- * bit operations and a certain number of them.
- *
- * There are three main categories of functions:
- *   (1) Bit manipulation - functions that manipulate & test sets of bits
- *   (2) Two's complement arithmetic - involve two's complement representations
- *   (3) Floating point arithmetic - operations on bit-level representations of
- *		 floating point numbers.
- *
- * Some selected examples:
- *   - bang(x) - Compute !x without using !
- *   - float_f2i(f) - Given a float f, compute (int) f
- *   - howManyBits(x) - Return the minimum number of bits needed to represent x
- *						in two's complement
- *
- * All functions have a header containing a description & examples. 
- *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 
